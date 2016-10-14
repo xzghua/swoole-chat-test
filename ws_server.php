e89b6c5c27ddc52aac5b0b36714bec6c12e8497e
Yela528 <2067930913@qq.com>
2016-10-14 18:34:01 +0800

    22

--- ws_server.php
+++ ws_server.php
@@ -7,7 +7,7 @@
  */
 
 //创建websocket服务器对象，监听0.0.0.0:9502端口
-$ws = new swoole_websocket_server("0.0.0.0", 9502);
+$ws = new swoole_websocket_server("127.0.0.1", 9502);
 
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
